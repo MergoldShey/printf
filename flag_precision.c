8b9ee403a58534b8a33c4c96d403fdd7a1f6b868
SuunyG <sundaygeorge247@gmail.com>
2022-08-11 11:17:07 -0700

    commpleted

--- flag_precision.c
+++ flag_precision.c
@@ -8,6 +8,7 @@
  *
  * Return: Precision.
  */
+
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
@@ -39,7 +40,6 @@ int get_precision(const char *format, int *i, va_list list)
 
 	return (precision);
 }
-
 
 
 /**
